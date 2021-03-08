02cdb8ac0cbec9c4f24351c5fb5767f042405328
James Wheadon <james.wheadon@yahoo.com>
2021-03-08 10:27:18 +0000

    basic file setup

---
+++ server/data.js
@@ -0,0 +1,9 @@
+const fish = [
+    { id: 1, name: 'Marlin', weight: 820, length: 5},
+    { id: 2, name: "Great White", weight: 1900, length: 4.9},
+    { id: 3, name: "Whale Shark", weight: 19000, length: 14.5},
+    { id: 4, name: "Barracuda", weight: 23, length: 1.5},
+    { id: 5, name: "Manta Ray", weight: 3000, length: 7}
+];
+
+module.exports = fish;
